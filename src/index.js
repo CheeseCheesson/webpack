77c9d9d08e575498d027c9f5d98af5fac884f89a
CheeseCheesson <kira7999@gmail.com>
2021-11-08 23:05:54 +0300

    4 exercise

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
-console.log('Hello, world');
+import css from "../style.css";
+import img from '../assets/js.jpg';
+
+const imageContainerHtml = document.querySelector('.myImage')
+
+const imgHTML = document.createElement('img')
+imgHTML.className = 'img-item'
+imgHTML.src = img
+
+imageContainerHtml.append(imgHTML)
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,50 @@
-const path = require('path');
+const path = require("path");
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
-  entry: './src/index.js',
+  mode: "development",
+  entry: "./src/index.js",
   output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, 'dist'),
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+    publicPath: '/',
+    clean: true
+  },
+  devServer: {
+    static: "./dist",
+    open: true,
+    compress: true,
+    port: 4000,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+        template: './index.html'
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(?:ico|gif|png|jpg|jpeg)$/i,
+        type: "asset/resource",
+      },
+      {
+        test: /\.(woff(2)?|eot|ttf|otf|svg|)$/,
+        type: "asset/inline",
+      },
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: [["@babel/preset-env", { targets: "defaults" }]],
+          },
+        },
+      },
+    ],
   },
 };
